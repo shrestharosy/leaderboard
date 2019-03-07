0caf0a71b6636928f0715a96c35706c1194a8869
Rosy <rosy@fusemachines.com>
2019-03-07 19:27:53 +0545

    [UPDATE] : Create components for sidebar and header

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Nav } from "reactstrap";
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.activeRoute.bind(this);
+  }
+  // verifies if routeName is the one active (in browser input)
+  activeRoute(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
+
+  render() {
+    return (
+      <div
+        className="sidebar"
+        data-color={this.props.bgColor}
+        data-active-color={this.props.activeColor}
+      >
+        <div className="logo">
+          <a
+            href="https://www.fusemachines.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="simple-text logo-normal"
+          >
+            FUSEMACHINES <br/>AI COMPETITION
+          </a>
+        </div>
+        <div className="sidebar-wrapper" ref="sidebar">
+          <Nav>
+            {this.props.routes.map((prop, key) => {
+              if (prop.redirect) return null;
+              return (
+                <li
+                  className={
+                    this.activeRoute(prop.path) +
+                    (prop.pro ? " active-pro" : "")
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </Nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
